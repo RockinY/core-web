d3ca50020990ba6c11393ee95f843725010cd774
Bran Liang <lby89757@hotmail.com>
2018-06-05 20:46:01 +0800

    Add footer

--- src/views/pages/components/footer.js
+++ src/views/pages/components/footer.js
@@ -9,3 +9,59 @@ import (
   SocialLinks
 ) from '../styles'
 import { Link } from 'react-router-dom'
+import { IconButton } from '../../../components/buttons'
+import { Logo } from '../../../components/logo'
+
+export default () => {
+  return (
+    <Footer>
+      <FooterGrid>
+        <Masthead>
+          <Link to="/">
+            <Logo />
+          </Link>
+          <SocialLinks>
+            <a
+              href="https://github.com/withspectrum"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <IconButton glyph="github" hoverColor={'text.reverse'} />
+            </a>
+            <a
+              href="https://twitter.com/withspectrum"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <IconButton glyph="twitter" hoverColor={'text.reverse'} />
+            </a>
+          </SocialLinks>
+        </Masthead>
+        <Support>
+          <span>Support</span>
+          <Link to={`/spectrum`}>Community</Link>
+          <Link to={`/spectrum/hugs-n-bugs`}>Bug reports</Link>
+          <Link to={`/spectrum/feature-requests`}>Feature requests</Link>
+          <a href="mailto:hi@spectrum.chat">Email support</a>
+        </Support>
+        <Safety>
+          <span>Safety</span>
+          <a
+            href="https://github.com/withspectrum/code-of-conduct"
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={() =>
+              track(events.CODE_OF_CONDUCT_CLICKED, {
+                location: 'splash page footer',
+              })
+            }
+          >
+            Code of Conduct
+          </a>
+          <Link to="/privacy">Privacy Policy</Link>
+          <Link to="/terms">Terms of Service</Link>
+        </Safety>
+      </FooterGrid>
+    </Footer>
+  )
+}
