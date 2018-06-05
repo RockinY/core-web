dfb05fafbfd12e46c017133a3c6cd7789716095f
Bran Liang <lby89757@hotmail.com>
2018-06-05 20:42:43 +0800

    Add react router dom

---
+++ src/views/pages/components/footer.js
@@ -0,0 +1,11 @@
+// @flow
+import React from 'react'
+import (
+  Footer,
+  FooterGrid,
+  Masthead,
+  Support,
+  Safety,
+  SocialLinks
+) from '../styles'
+import { Link } from 'react-router-dom'
--- src/views/pages/home/index.js
+++ src/views/pages/home/index.js
@@ -1,5 +1,8 @@
 // @flow
 import React from 'react'
+import { storeItem, getItemFromStorage } from '../../../helpers/localStorage'
+import PageFooter from '../components/footer'
+import { Wrapper } from '../style'
 
 class Splash extends React.Component<{}> {
   render () {
