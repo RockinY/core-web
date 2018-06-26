3f78883e5c459a012375a95ba44e5bcf41a89980
Bran Liang <lby89757@hotmail.com>
2018-06-26 21:14:55 +0800

    Add editCommunityForm

--- src/graphql/queries/community/getCommunity.js
+++ src/graphql/queries/community/getCommunity.js
@@ -1,11 +1,29 @@
 import gql from 'graphql-tag'
 import communityInfoFragment from '../../fragments/community/communityInfo'
 import communityMetaDataFragment from '../../fragments/community/communityMetaData'
+import type { CommunityInfoType } from '../../fragments/community/communityInfo'
+import type { CommunityMetaDataType } from '../../fragments/community/communityMetaData'
+
+export type GetCommunityType = {
+  ...$Exact<CommunityInfoType>,
+  ...$Exact<CommunityMetaDataType>
+}
 
 export const getCommunityBySlugQuery = gql`
   query getCommunityBySlug($slug: LowercaseString) {
     community(slug: $slug) {
       ...communityInfo
+      ...communityMetaData
+    }
+  }
+  ${communityInfoFragment}
+  ${communityMetaDataFragment}
+`
+
+export const getCommunityByIdQuery = gql`
+  query getCommunityById($id: ID) {
+    community(id: $id) {
+      ...communityInfo,
       ...communityMetaData
     }
   }
