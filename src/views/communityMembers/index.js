94393c2cf7dddec291f527a578b06d14e715b82e
Bran Liang <lby89757@hotmail.com>
2018-07-09 19:30:42 +0800

    Add view community member

---
+++ src/views/communityMembers/index.js
@@ -0,0 +1,81 @@
+// @flow
+import * as React from 'react';
+import compose from 'recompose/compose';
+import { connect } from 'react-redux';
+import Link from '../../components/link';
+import type { GetCommunityType } from '../../graphql/queries/community/getCommunity';
+import ViewError from '../../components/viewError';
+import { Button, OutlineButton, ButtonRow } from '../../components/buttons';
+import CommunityMembers from './components/communityMembers';
+import JoinTokenSettings from './components/joinTokenSettings';
+import type { Dispatch } from 'redux';
+import {
+  SectionsContainer,
+  SectionCard,
+  SectionTitle,
+  Column,
+} from '../../components/settingsViews/style';
+import { ErrorBoundary, SettingsFallback } from '../../components/error';
+
+type Props = {
+  currentUser: Object,
+  community: GetCommunityType,
+  dispatch: Dispatch<Object>,
+  match: Object,
+  history: Object,
+};
+
+class CommunityMembersSettings extends React.Component<Props> {
+  render() {
+    const { community, history } = this.props;
+
+    if (community && community.id) {
+      return (
+        <SectionsContainer>
+          <Column>
+            <ErrorBoundary fallbackComponent={SettingsFallback}>
+              <CommunityMembers
+                history={history}
+                id={community.id}
+                community={community}
+              />
+            </ErrorBoundary>
+          </Column>
+
+          <Column>
+            {community.isPrivate && (
+              <ErrorBoundary fallbackComponent={SettingsFallback}>
+                <JoinTokenSettings id={community.id} community={community} />
+              </ErrorBoundary>
+            )}
+          </Column>
+        </SectionsContainer>
+      );
+    }
+
+    return (
+      <ViewError
+        heading={'You don’t have permission to manage this community.'}
+        subheading={
+          'If you want to create your own community, you can get started below.'
+        }
+      >
+        <ButtonRow>
+          <Link to={'/'}>
+            <OutlineButton large>Take me back</OutlineButton>
+          </Link>
+
+          <Link to={'/new/community'}>
+            <Button large>Create a community</Button>
+          </Link>
+        </ButtonRow>
+      </ViewError>
+    );
+  }
+}
+
+const map = state => ({ currentUser: state.users.currentUser });
+export default compose(
+  // $FlowIssue
+  connect(map)
+)(CommunityMembersSettings);
