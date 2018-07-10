02e7ed4080048ac4dbd8546d96a20d584f7380d0
Bran Liang <lby89757@hotmail.com>
2018-07-10 20:42:53 +0800

    Add component modal

---
+++ src/components/modals/modalRoot.js
@@ -0,0 +1,39 @@
+// @flow
+import React from 'react';
+import { connect } from 'react-redux';
+import CreateChannelModal from './CreateChannelModal';
+import DeleteDoubleCheckModal from './DeleteDoubleCheckModal';
+import RepExplainerModal from './RepExplainerModal';
+import ChangeChannelModal from './ChangeChannelModal';
+import RestoreChannelModal from './RestoreChannelModal';
+import ChatInputLoginModal from './ChatInputLoginModal';
+
+const MODAL_COMPONENTS = {
+  CREATE_CHANNEL_MODAL: CreateChannelModal,
+  DELETE_DOUBLE_CHECK_MODAL: DeleteDoubleCheckModal,
+  REP_EXPLAINER_MODAL: RepExplainerModal,
+  CHANGE_CHANNEL: ChangeChannelModal,
+  RESTORE_CHANNEL_MODAL: RestoreChannelModal,
+  CHAT_INPUT_LOGIN_MODAL: ChatInputLoginModal
+};
+
+/*
+  Takes a modalType and modalProps to dynamically return the
+  modal component we imported above
+*/
+const modalRoot = ({ modalType, modalProps }) => {
+  if (!modalType) {
+    return null;
+  }
+
+  const SpecificModal = MODAL_COMPONENTS[modalType];
+  return <SpecificModal {...modalProps} />;
+};
+
+const mapStateToProps = state => ({
+  modalProps: state.modals.modalProps,
+  modalType: state.modals.modalType,
+});
+
+// $FlowIssue
+export default connect(mapStateToProps)(modalRoot);
