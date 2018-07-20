6a88eb52f9599b6801458249413dc75477128ba9
Bran Liang <lby89757@hotmail.com>
2018-07-20 23:56:56 +0800

    Add redis config

--- src/ssr/redis.js
+++ src/ssr/redis.js
@@ -1,32 +1,8 @@
 // @flow
-import createRedis from 'shared/bull/create-redis';
-
-const config =
-  process.env.NODE_ENV === 'production' && !process.env.FORCE_DEV
-    ? {
-        port: process.env.REDIS_CACHE_PORT,
-        host: process.env.REDIS_CACHE_URL,
-        password: process.env.REDIS_CACHE_PASSWORD,
-      }
-    : undefined;
-
-// Turn the NOW_URL into the deployment ID
-// https://spectrum-asdf123.now.sh -> spectrum-asdf123
-const deploymentId =
-  process.env.NOW_URL &&
-  process.env.NOW_URL.replace(/^https:\/\//, '').replace(/\.now\.sh$/, '');
-// Locally key the cache only with "cache:", when deployed key the cache with the
-// deployment's NOW_URL to avoid serving HTML that refers to non-existant scripts.
-// e.g. "cache:spectrum-asdf123:"
-const getKeyPrefix = () => {
-  if (!deploymentId) return 'cache:';
-
-  return `cache:${deploymentId}:`;
-};
+import createRedis from '../utils/createRedis'
 
 const redis = createRedis({
-  keyPrefix: getKeyPrefix(),
-  ...config,
-});
+  keyPrefix: process.env.REDIS_CACHE_PREFIX
+})
 
-export default redis;
+export default redis
