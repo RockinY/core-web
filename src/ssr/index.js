daf897c714fa279019528adfbf69bd56f949575b
Bran Liang <lby89757@hotmail.com>
2018-07-21 13:55:27 +0800

    Fullfill the ssr server

--- src/ssr/index.js
+++ src/ssr/index.js
@@ -9,83 +9,55 @@ import express from 'express';
 import Loadable from 'react-loadable';
 import path from 'path';
 import { getUser } from './model';
-import Raven from '../utils/raven';
+import Raven from './raven';
 import toobusy from './middlewares/toobusy';
 import addSecurityMiddleware from './middlewares/security';
+import logging from './middlewares/logging'
+import session from './middlewares/session'
+import cors from 'cors'
+import passport from 'passport'
+import cookieParser from 'cookie-parser'
+import bodyParser from 'body-parser'
+import threadParamRedirect from './middlewares/threadParam'
 
-const PORT = process.env.PORT || 3006;
+const PORT = process.env.SSR_PORT || 3006;
+const serverUrl = process.env.REACT_APP_SERVER_URL || 'http://localhost:3000'
 
-const app = express();
+/* ----------- API server ----------- */
+const app = express()
+app.set('trust proxy', true)
 
-// Trust the now proxy
-app.set('trust proxy', true);
-
-app.use(toobusy);
-
-// Security middleware.
-addSecurityMiddleware(app);
-
+/* ----------- Middlewares ----------- */
+// *. Toobusy situation
+app.use(toobusy)
+// *. Increase security
+addSecurityMiddleware(app)
+// *. Logging
 if (process.env.NODE_ENV === 'development') {
-  const logging = require('shared/middlewares/logging');
-  app.use(logging);
+  app.use(logging)
 }
+// *. Cross Origin Request
+app.use(cors({
+  origin: ['http://localhost:5000', 'https://www.liangboyuan.pub'],
+  credentials: true
+}))
 
-if (process.env.NODE_ENV === 'production' && !process.env.FORCE_DEV) {
-  // Raven (Sentry client) needs to come before everything else
-  const raven = require('shared/middlewares/raven').default;
-  app.use(raven);
-}
-
-// Cross origin request support
-import cors from 'shared/middlewares/cors';
-app.use(cors);
-
-// Redirect requests to /api and /auth to the production API
-// This allows deploy previews to work, as this route would only be called
-// if there's no path alias in Now for hyperionurl.com/api, which would only
-// happen on deploy previews
-app.use('/api', (req: express$Request, res: express$Response) => {
+// Redirect requests to /api, /auth and /websocket to the server API
+app.use(['/api', '/auth', '/websocket'], (req: express$Request, res: express$Response) => {
   const redirectUrl = `${req.baseUrl}${req.path}`;
   res.redirect(
     req.method === 'POST' || req.xhr ? 307 : 301,
-    `https://spectrum.chat${redirectUrl}`
+    `${serverUrl}${redirectUrl}`
   );
-});
+})
 
-app.use('/auth', (req: express$Request, res: express$Response) => {
-  const redirectUrl = `${req.baseUrl}${req.path}`;
-  res.redirect(
-    req.method === 'POST' || req.xhr ? 307 : 301,
-    `https://spectrum.chat${redirectUrl}`
-  );
-});
+// *. Cookie parser
+app.use(cookieParser())
+// *. JSON body parser
+app.use(bodyParser.json())
+// *. Session
+app.use(session)
 
-app.use('/websocket', (req: express$Request, res: express$Response) => {
-  const redirectUrl = `${req.baseUrl}${req.path}`;
-  res.redirect(
-    req.method === 'POST' || req.xhr ? 307 : 301,
-    `https://spectrum.chat${redirectUrl}`
-  );
-});
-
-// In development the Webpack HMR server requests /sockjs-node constantly,
-// so let's patch that through to it!
-if (process.env.NODE_ENV === 'development') {
-  app.use('/sockjs-node', (req: express$Request, res: express$Response) => {
-    res.redirect(301, `http://localhost:3000${req.path}`);
-  });
-}
-
-import cookieParser from 'cookie-parser';
-app.use(cookieParser());
-
-import bodyParser from 'body-parser';
-app.use(bodyParser.json());
-
-import session from 'shared/middlewares/session';
-app.use(session);
-
-import passport from 'passport';
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
@@ -103,7 +75,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // This needs to come after passport otherwise we'll always redirect logged-in users
-import threadParamRedirect from 'shared/middlewares/thread-param';
 app.use(threadParamRedirect);
 
 // Static files
@@ -194,6 +165,6 @@ process.on('uncaughtException', async err => {
 Loadable.preloadAll().then(() => {
   app.listen(PORT);
   debug(
-    `Hyperion, the server-side renderer, running at http://localhost:${PORT}`
+    `Server-side renderer, running at http://localhost:${PORT}`
   );
 });
