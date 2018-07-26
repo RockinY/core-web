455638a2d7044a58ce349dc7c0bae15a98309f2d
Bran Liang <lby89757@hotmail.com>
2018-07-26 21:47:39 +0800

    Add center addon for storybook

--- stories/components/avatar.stories.js
+++ stories/components/avatar.stories.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import Avatar from '../../src/components/avatar'
+import centered from '@storybook/addon-centered'
 
 storiesOf('Components|Avatar', module)
+  .addDecorator(centered)
   .add('Default', () => (
     <div>Test</div>
   ))
