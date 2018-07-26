80bb55ab15f9fa7bb1e9bc249f7a1c08ec72331a
Bran Liang <lby89757@hotmail.com>
2018-07-26 18:08:34 +0800

    Update the graphql

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,19 @@
 import { configure } from '@storybook/react'
 
+function importAll(req) {
+  req.keys().forEach(filename => req(filename));
+}
+
 function loadStories() {
-  require('../stories/index.js')
+  let req;
+
+  // Graphql
+  req = require.context('../stories/graphql', true, /\.stories\.js$/);
+  importAll(req);
+
+  // Compoents
+  req = require.context('../stories/components', true, /\.stories\.js$/);
+  importAll(req);
 }
 
 configure(loadStories, module)
